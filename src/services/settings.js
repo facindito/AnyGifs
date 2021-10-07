113a67fb23abcc8b66a960dbf48741d2dc5f06c5
facindito <facinditto@gmail.com>
2021-10-07 18:08:54 -0300

    Add Environment Varibles

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,4 +1,4 @@
 
-export const API_KEY = process.env.REACT_APP_API_KEY
+export const API_KEY = process.env.REACT_APP_API_KEY || process.env.VERCEL_APP_API_KEY
 
 export const API_URL = `https://api.giphy.com/v1`
