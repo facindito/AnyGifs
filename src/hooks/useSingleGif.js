25912d1905cdd1accf70ee85db90ec6e749e20c4
facindito <facinditto@gmail.com>
2022-02-04 18:32:16 -0300

    changes

--- src/hooks/useSingleGif.js
+++ src/hooks/useSingleGif.js
@@ -1,5 +1,5 @@
 import { useGifs } from "hooks/useGifs";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import getSingleGif from "services/getSingleGif";
 
 
